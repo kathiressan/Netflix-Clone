acabcf8db001f820eb744fb5c2463b61b31d545a
Kathiressan Sivanes <kathiressansivanes26@gmail.com>
2021-07-14 14:49:17 +0800

    First Commit
    - Added the main homepage of the Netflix clone
    - Used the TMDB API to get all the movies data

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNUZRn51UvJLqWz_YjZaMJ5aDfWDakAKI",
+  authDomain: "netflix-clone-9a9e7.firebaseapp.com",
+  projectId: "netflix-clone-9a9e7",
+  storageBucket: "netflix-clone-9a9e7.appspot.com",
+  messagingSenderId: "676367080416",
+  appId: "1:676367080416:web:49bd45e43f2620bf0ce985",
+};
